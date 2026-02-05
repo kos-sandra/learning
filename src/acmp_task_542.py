48d76934d46f3c4689583e8c0da3abf0a4a0a030
kos-sandra <kos-sandra@ya.ru>
2026-02-05 19:26:13 +0300

    task 542

--- src/acmp_task_542.py
+++ src/acmp_task_542.py
@@ -1,19 +1,25 @@
 def to_dexi(n):
-    n = str(n)
     z = len(n)
-    p = list([int(x) for x in n])
-
     res = 0
     j = 0
+
     for i in range(z - 1, -1, -1):
-        res += p[j] * 2 ** i
+        res += n[j] * 2 ** i
         j += 1
     return res
 
-def
+def to_binary(n):
+    res = []
+    while n >= 2:
+        res.append(n%2)
+        n //= 2
+    res.append(n)
+    return res
 
+n = int(input())
+binary_list = to_binary(n)
+print(to_dexi(binary_list))
 
-n = input()
 
 
 
