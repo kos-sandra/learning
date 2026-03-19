1762ce33017ee165146645e376b1d893e3c286ea
kos-sandra <kos-sandra@ya.ru>
2026-03-19 20:45:35 +0300

    task 517 done

--- acmp/acmp_task_517.py
+++ acmp/acmp_task_517.py
@@ -2,9 +2,18 @@ n = int(input())
 score = [int(x) for x in input().split()]
 total = 0
 
-tour = 0
-while True:
+i = 0
 
-for i,x in enumerate(score):
-    if x == 10:
-        total += 10 + score[i+1] + score[i+2]
+for t in range(10):
+    total += score[i]
+    strike = score[i] == 10
+    spare = score[i] + score[i + 1] == 10 and not strike
+
+    if strike:
+        total += score[i + 1] + score[i + 2]
+        i += 1
+    else:
+        total += score[i+1] if not spare else score[i + 1] + score[i + 2]
+        i += 2
+
+print(total)
